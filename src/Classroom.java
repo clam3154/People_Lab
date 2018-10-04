be4b5e8957bb5f3f65aa823858cb3c6647430a81
chrisss101 <Crafaniello9535@bths.edu>
2018-10-04 09:52:33 -0400

    dtu6dty

---
+++ src/Classroom.java
@@ -0,0 +1,23 @@
+public abstract class Classroom  {
+    private Person[] students;
+    private Person teacher;
+    public Classroom(Person[] students, Person teacher ) {
+
+        this.students = students;
+        this.teacher = teacher;
+    }
+
+    public String getSubject(String Subject) {
+        this.Subject = Subject;
+    }
+    public String classAverage() {
+
+    }
+
+    public String printClass() {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
