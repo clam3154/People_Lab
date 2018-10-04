298f1c420f029bdbf2312de386ad9fcb0ef515eb
chrisss101 <Crafaniello9535@bths.edu>
2018-10-04 10:00:28 -0400

    dtu6dty

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,23 +1,30 @@
 public abstract class Classroom  {
     private Person[] students;
     private Person teacher;
-    public Classroom(Person[] students, Person teacher ) {
-
+    public Classroom(Person[] students, Person teacher )
+    {
         this.students = students;
         this.teacher = teacher;
     }
 
-    public String getSubject(String Subject) {
-        this.Subject = Subject;
-    }
-    public String classAverage() {
+    public String getSubject(String Subject)
+    {
 
     }
+    public String classAverage()
+    {
+        int totalGPA = 0;
+        for (int i = 0; i < students.length - 1; i++) {
+            totalGPA += students[i].GPA ;
+        }
+    }
 
-    public String printClass() {
+    public String printClass()
+    {
 
     }
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
     }
 }
