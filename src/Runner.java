c40e8e17665c78983dbc4bc70d0d062a4b3d4c00
BT_1N3_31 <cyang4980@bths.edu>
2018-10-04 09:59:53 -0400

    .

--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,11 @@
 public class Runner {
-    static String[] firstNames;
-    static String[] familyNames;
 
+    private static String[] firstNames = {"Abby", "Derick", "Elijah", "Fin", "Gillian", "Helen", "Leon", "John", "Kenny", "Matthew", "Nate", "Priscilla"};
+    private static String[] familyNames = {"McDonalds", "Anderson", "Smith", "Stevens", "Lee", "Chen"};
+    private static String[] majors = {"Software Engineering", "Math", "Chemistry", "Biology", "Visual Art", "Media", "Fashion Design", "Metaphysics", "Astrology", "English", "Robotics"};
 
     public static void main(String[] args)
     {
-        Person teacher = new Teacher
+    //    Person teacher = new Teacher
     }
 }
